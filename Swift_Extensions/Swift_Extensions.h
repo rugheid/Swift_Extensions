7186f45b5069a4e44d6a05036edb5da8b9a5f604
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2014-10-28 15:43:48 +0100

    Initial commit from local git

---
+++ Swift_Extensions/Swift_Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Extensions.h
+//  Swift_Extensions
+//
+//  Created by Rugen Heidbuchel on 28/10/14.
+//  Copyright (c) 2014 Jorestha Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift_Extensions.
+FOUNDATION_EXPORT double Swift_ExtensionsVersionNumber;
+
+//! Project version string for Swift_Extensions.
+FOUNDATION_EXPORT const unsigned char Swift_ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Extensions/PublicHeader.h>
+
+
